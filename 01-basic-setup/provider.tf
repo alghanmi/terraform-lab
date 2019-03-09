5ffa2d50bb2b6ff8f7697972cbe9e7549de2ff12
Rami Al-Ghanmi <rami@ramialghanmi.com>
2019-03-09 08:04:08 -0800

    update dynamodb state-lock table name and s3 bucket name

--- 01-basic-setup/provider.tf
+++ 01-basic-setup/provider.tf
@@ -6,11 +6,11 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "sgvlug-terraform-state"
+    bucket         = "tflab-terraform-statelock-416373849276"
     key            = "terraform-lab/basic"
     region         = "us-west-2"
     profile        = "scale"
     encrypt        = true
-    dynamodb_table = "sgvlug-terraform-statelock-bucket"
+    dynamodb_table = "tflab-terraform-statelock"
   }
 }
