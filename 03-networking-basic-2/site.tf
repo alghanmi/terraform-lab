239d0f66e44017a0dc3a36661be78ecf19b9aabd
Rami Al-Ghanmi <rami@ramialghanmi.com>
2019-03-09 09:10:38 -0800

    fix az for subnet

--- 03-networking-basic-2/site.tf
+++ 03-networking-basic-2/site.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "main" {
   cidr_block              = "10.0.0.0/24"
   vpc_id                  = "${aws_vpc.main.id}"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-west-2a"
 
   tags {
     Name = "main-vpc-subnet-01"
